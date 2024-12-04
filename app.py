5425c8813e0b7e876a1e2bb110de3613e9f04966
Roseyshi <136338526+Roseyshi@users.noreply.github.com>
2024-12-04 23:28:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 import streamlit as st
-import joblib
+import pickle
 
-# Load model
-model = joblib.load('credit_model.pkl')
+# Load model using pickle
+with open('credit_model.pkl', 'rb') as f:
+    model = pickle.load(f)
 
 # App title
 st.title("Credit Score Prediction App")
 
 # Input fields
-feature1 = st.number_input("Outstanding_Debt")
-feature2 = st.number_input("Credit_Mix")
-feature3 = st.number_input("Credit_History_Age_in_months")
-feature4 = st.number_input("Monthly_Balance")
-feature5 = st.number_input("Paymnet_Behaviour")
-feature6 = st.number_input("Annual_Income")
-feature7 = st.number_input("Number_of_Delayed_Payment")
-
-
+feature1 = st.number_input("Outstanding Debt")
+feature2 = st.number_input("Credit Mix")
+feature3 = st.number_input("Credit History Age (in months)")
+feature4 = st.number_input("Monthly Balance")
+feature5 = st.number_input("Payment Behaviour")
+feature6 = st.number_input("Annual Income")
+feature7 = st.number_input("Number of Delayed Payments")
 
 if st.button("Predict"):
     prediction = model.predict([[feature1, feature2, feature3, feature4, feature5, feature6, feature7]])
-    st.write(f"Predicted Class: {data.target_names[prediction[0]]}")
+    st.write(f"Predicted Class: {prediction[0]}")
+
