053f7e7df07adcd013bedee59be7f037283fcd63
Roseyshi <136338526+Roseyshi@users.noreply.github.com>
2024-12-04 23:04:35 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import joblib
+
+# Load model
+model = joblib.load('credit_model.pkl')
+
+# App title
+st.title("Credit Score Prediction App")
+
+# Input fields
+feature1 = st.number_input("Outstanding_Debt")
+feature2 = st.number_input("Credit_Mix")
+feature3 = st.number_input("Credit_History_Age_in_months")
+feature4 = st.number_input("Monthly_Balance")
+feature5 = st.number_input("Paymnet_Behaviour")
+feature6 = st.number_input("Annual_Income")
+feature7 = st.number_input("Number_of_Delayed_Payment")
+
+
+
+if st.button("Predict"):
+    prediction = model.predict([[feature1, feature2, feature3, feature4, feature5, feature6, feature7]])
+    st.write(f"Predicted Class: {data.target_names[prediction[0]]}")
